e8c088afacdd16e5013c83a8e37243491b87f496
Joseph E <1jojorun@gmail.com>
2023-03-27 08:30:26 -0700

    Set up divisions for home page of application

--- front-end/src/components/Home.js
+++ front-end/src/components/Home.js
@@ -8,13 +8,21 @@ const Home = () => {
       <div>
         <h2>Dollars Bank</h2>
         <div id="button-container">
-          <i className="fas fa-envelope-square" style={{fontSize:"24px"}}></i>
+            <i className="fas fa-user-alt" style={{
+              fontSize: "18px",
+              marginLeft: "5px" 
+            }}></i>
           <button type="button" class="btn btn-danger">Login</button>
           <button type="button" class="btn btn-danger">Register</button>
         </div>
       </div>
       <div>
         <img src={rr} alt="Money Meme"></img>
+        <div>
+          <p>Pioneers of Banking Worldwide!</p>
+          <p>Rated by our global customers</p>
+          <p>Enjoy banking with us!</p>
+        </div>
         <hr/>
       </div>
     </div>
